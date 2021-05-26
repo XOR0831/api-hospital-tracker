e49355b503c9b4359d2cc3899b211c65e2613a3c
XOR0831 <sorianodanielb99@gmail.com>
2021-05-27 00:52:59 +0800

    Added Debug to Heroku Config Vars

--- api_hospital_tracker/settings.py
+++ api_hospital_tracker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ii##fxhy##a!hh^rrrq*9y&n+(!n8rb&4t$u*51^5niiouh$3#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
